2cea1a1ff80465e5b283a3e1e2651078158370cb
Christianngono <christianngonoabanda384@gmail.com>
2025-02-19 13:26:25 +0100

    To ameliore Score.php

---
+++ habit-tracker/src/Entity/Score.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ScoreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ScoreRepository::class)]
+class Score
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'integer')]
+    private ?int $value = null; // Le score réel
+    
+    #[ORM\Column(type: 'datetime')]
+    private ?\DateTimeInterface $dateCreated = null; // Date de création du score
+
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null; // L'utilisateur auquel ce score appartient
+
+    public function __construct()
+    {
+        $this->dateCreated = new \DateTime(); // La date de création est par défaut la date actuelle
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(int $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getDateCreated(): ?\DateTimeInterface
+    {
+        return $this->dateCreated;
+    }
+
+    public function setDateCreated(\DateTimeInterface $dateCreated): self
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
