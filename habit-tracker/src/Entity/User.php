d95da81e78b942a252732bed95bb74ed94933968
Christianngono <christianngonoabanda384@gmail.com>
2025-02-19 08:35:54 +0100

    To add User.php

---
+++ habit-tracker/src/Entity/User.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User implements UserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private?string $pseudo = null;
+
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $password = null;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private?string $photo = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+
+
+    public function getPseudo():?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(?string $pseudo): self
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function getEmail():?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+    public function getPassword():?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(?string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getPhoto():?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * Return an array of roles granted to the user.
+     *
+     * @see UserInterface
+     */
+
+    public function getRoles(): array
+    {
+        return ['ROLE_USER'];
+    }
+
+    /**
+     * Erase sensitive data,  temporary password.
+     */
+
+    
+    public function eraseCredentials(): void
+    {
+        // Effacer les données temporaires comme le mot de passe
+    }
+
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+    /**
+     * Returns the username (email).
+     */
+    public function getUsername(): string
+    {
+        return $this->email;
+    }
+}
